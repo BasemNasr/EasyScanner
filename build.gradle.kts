884a10d7ca30941f883acd9cfa13c7eb4ae0b2c0
a1 <basemads20@gmail.com>
2023-10-23 14:32:54 +0300

    add publishing maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,27 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
 }
+
+allprojects {
+    repositories {
+        google()
+        maven(url = "https://jitpack.io")
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+
+
+
